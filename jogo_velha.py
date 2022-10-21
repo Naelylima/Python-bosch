0f797c42d3a414c8f674715e68872ae85518c90e
Naely Lima Pereira <110494299+Naelylima@users.noreply.github.com>
2022-10-21 11:24:15 -0300

    Update jogo_velha.py

--- jogo_velha.py
+++ jogo_velha.py
@@ -24,6 +24,7 @@ def menu_escolha(jogador_1, jogador_2):
                 jogador_2 = "O"
                 print("-"*38)
                 print(f"Jogador 1 neste jogo você será: {jogador_1}\nJogador 2 neste jogo você será: {jogador_2}")
+                print("-" * 38)
                 return jogador_1, jogador_2
             elif jogador_1 == 3:
                 print("-" * 38)
@@ -35,95 +36,102 @@ def menu_escolha(jogador_1, jogador_2):
                 jogador_2 = "X"
                 print("-" * 38)
                 print(f"Jogador 1 neste jogo você será: {jogador_1}\nJogador 2 neste jogo você será: {jogador_2}")
+                print("-" * 38)
                 return jogador_1, jogador_2
         except ValueError:
             print("Você digitou algo errado\n")
             continue
 
 def verificar(par):
-    for i in lista_posicoes:
-        for j in i:
-            if par == j:
-                return True
-            else:
-                return False
+    for i in range(3):
+        for j in range(3):
+            if par == lista_posicoes[i][j]:
+                return i, j
+    return None, None
 
-def jogadas1(jogada1):
-    print(verificar(0))
-    if jogada1==0:
-        for linha in range(0, 3):
-            if  not verificar(jogada1):
-                print(lista_posicoes[linha])
-                print("if1")
-            else:
-                print("else1")
-                if jogada1 in lista_posicoes[linha]:
-                    posicao = lista_posicoes[linha].index(jogada1)
-                    lista_posicoes[linha][posicao] = jogador1
-                    vitoria(vencer=venceu)
-                    break
-    else:
-        for linha in range(0, 3):
-            if  verificar(jogada1):
-                print(lista_posicoes[linha])
-                print("if1")
-            else:
-                print("else1")
-                if jogada1 in lista_posicoes[linha]:
-                    posicao = lista_posicoes[linha].index(jogada1)
-                    lista_posicoes[linha][posicao] = jogador1
-                    vitoria(vencer=venceu)
-                    break
-    desenho_forca(lista_posicoes)
-
-
-
-def jogadas2(jogada2):
-    for linha in range(0, 3):
-        if verificar(jogada2):
-            print("if2")
+def jogadas(jogada1, jogador):
+    vencer = False
+    linha, coluna = verificar(jogada1)
+    if linha != None:
+        if jogador == 1:
+            lista_posicoes[linha][coluna] = jogador1
         else:
-            print("else2")
-            if jogada2 in lista_posicoes[linha]:
-                posicao = lista_posicoes[linha].index(jogada2)
-                lista_posicoes[linha][posicao] = jogador2
-                vitoria(vencer=venceu)
-                break
+            lista_posicoes[linha][coluna] = jogador2
+        vencer = vitoria(vencer=venceu)
+    else:
+        print("Essa opção já foi escolhida")
+        seguir_jogo = False
+        return seguir_jogo
+
     desenho_forca(lista_posicoes)
+    return vencer
 
 def vitoria(vencer):
+    vencer = False
     for linhas in range(0, 3):
         if lista_posicoes[linhas][0] == jogador1 and lista_posicoes[linhas][1] == jogador1 and lista_posicoes[linhas][2] == jogador1:
+            print("*" * 30)
             print("Jogador 1, VENCEU")
+            print("*" * 30)
             vencer = True
             return vencer
         if lista_posicoes[linhas][0] == jogador2 and lista_posicoes[linhas][1] == jogador2 and lista_posicoes[linhas][2] == jogador2:
+            print("*" * 30)
             print("Jogador 2, VENCEU")
+            print("*" * 30)
             vencer = True
             return vencer
     for linhas in range(0, 3):
         if lista_posicoes[0][linhas] == jogador1 and lista_posicoes[1][linhas] == jogador1 and lista_posicoes[2][linhas] == jogador1:
+            print("*" * 30)
             print("Jogador 1, VENCEU")
+            print("*" * 30)
             vencer = True
             return vencer
         if lista_posicoes[0][linhas] == jogador2 and lista_posicoes[1][linhas] == jogador2 and lista_posicoes[2][linhas] == jogador2:
+            print("*" * 30)
             print("Jogador 2, VENCEU")
+            print("*" * 30)
             vencer = True
             return vencer
+    if lista_posicoes[0][0] == jogador1 and lista_posicoes[1][1] == jogador1 and lista_posicoes[2][2] == jogador1:
+        print("*"*30)
+        print("Jogador 1, VENCEU")
+        print("*" * 30)
+        vencer = True
+        return vencer
+    elif lista_posicoes[0][0] == jogador2 and lista_posicoes[1][1] == jogador2 and lista_posicoes[2][2]== jogador2:
+        print("*" * 30)
+        print("Jogador 2, VENCEU")
+        print("*" * 30)
+        vencer = True
+        return vencer
+
+    return vencer
 
 if __name__ == "__main__":
     lista_posicoes = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
     menu_principal()
+    seguir_jogo = True
+    venceu = False
     jogador2 = ""
     jogador1 = int(input("Jogador 1, Digite o que será nesse jogo: "))
     jogador1, jogador2 = menu_escolha(jogador1, jogador2)
     desenho_forca(lista_posicoes)
-    venceu = False
     while True:
-        if venceu == True:
-            break
         venceu = vitoria(venceu)
-        jogada1 = int(input("Jogador 1, digite a posição deseja jogar:  "))
-        jogadas1(jogada1)
-        jogada2 = int(input("Jogador 2, digite a posição deseja jogar:  "))
-        jogadas2(jogada2)
+        while True:
+            if venceu == True:
+                break
+            else:
+                jogada1 = int(input("Jogador 1, digite a posição deseja jogar:  "))
+                venceu = jogadas(jogada1, 1)
+                if venceu == True:
+                    break
+                venceu = jogada2 = int(input("Jogador 2, digite a posição deseja jogar:  "))
+                jogadas(jogada2, 2)
+                if venceu == True:
+                    break
+        break
+
+
