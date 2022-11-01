a7b4299e7b3ee9e9f3a6a505615350a85ff4bf1b
Naely Lima Pereira <110494299+Naelylima@users.noreply.github.com>
2022-11-01 11:19:51 -0300

    Update jogo_dados.py

--- jogo_dados.py
+++ jogo_dados.py
@@ -2,20 +2,98 @@ import random
 import time
 
 
-class Jogo :
-    def __init__(self, nome, creditos ):
+class Jogo:
+    def __init__(self, nome, creditos, revanche1=False):
         self.nome = nome
         self.creditos = creditos
+        self.revanche1 = revanche1
 
 
 def inicio():
-    lista = [1, 2, 3, 4,5 ,6]
+    lista = [1, 2, 3, 4, 5, 6]
     print("\n-------------------- O JOGO VAI COMEÇAR --------------------")
     print(f"Bem vindo(a) {jogador.nome}!\nVocê inicia a partida com R${jogador.creditos} de créditos.")
     num_jogador = int(input(f"{jogador.nome}, digite o valor do dado (1-6): "))
     while True:
-        if num_jogador >6 or num_jogador <1:
-            num_jogador= int(input("Digite um valor de 1-6: "))
+        if num_jogador > 6 or num_jogador < 1:
+            num_jogador = int(input("Digite um valor de 1-6: "))
+        else:
+            print("O computador está escolhendo um número ...")
+            time.sleep(1)
+        if lista.__contains__(num_jogador):
+            if jogador.revanche1 == False:
+                jogador.creditos -= 2
+                lista.remove(num_jogador)
+                num_comp = random.choice(lista)
+                print(
+                    f'\n{jogador.nome}, seu número é: {num_jogador} e número escolhido pelo computador é: {num_comp}.')
+                time.sleep(1)
+                jogar_dados(num_jogador, num_comp)
+                break
+            else:
+                lista.remove(num_jogador)
+                num_comp = random.choice(lista)
+                print(f'\n{jogador.nome}, seu número é: {num_jogador} e número escolhido pelo computador é: {num_comp}.')
+                time.sleep(1)
+                jogar_dados(num_jogador, num_comp)
+                break
+
+
+def jogar_dados(num_jogador, num_comp):
+    print("\n ---------- OS DADOS FORAM LANÇADOS ---------- ")
+    time.sleep(1)
+    print("             *    SORTEANDO    *                 ")
+    time.sleep(2)
+    num_dados = 1
+    print(f'************ O numero do dado é: {num_dados} *************')
+    print('-' * 48)
+    verificar(num_dados, num_jogador, num_comp)
+
+
+def verificar(num_dados, num_jogador, num_comp):
+    if num_dados == num_jogador:
+        if jogador.revanche1 ==True:
+            print(f'{jogador.nome}, VOCÊ VENCEU !!!')
+            jogador.creditos += 8
+            print(f'Você ganhou R$8,00 de Crédito. Seu crédito atual é de: {jogador.creditos}')
+            menu()
+        if jogador.revanche1 == False:
+            print(f'{jogador.nome}, VOCÊ VENCEU !!!')
+            jogador.creditos += 4
+            print(f'Você ganhou R$ 4,00 de Crédito. Seu crédito atual é de: {jogador.creditos}')
+            revanche()
+
+    elif num_dados == num_comp:
+        print('O Computador venceu essa partida !! ')
+        menu()
+        print('Ninguém ganhou essa rodada, os dados serão jogados novamente')
+    else:
+        time.sleep(3)
+        jogar_dados(num_jogador, num_comp)
+
+
+def revanche():
+    print('\nO computador sugeriu uma revanche! O dobro ou nada')
+    print('Se você vencer você ganha mais R$ 4,00 de créditos para jogar')
+    resposta = int(input('\n[1] Sim'
+                         '\n[2] Não'
+                         '\nDigite o número referente a sua resposta:'))
+    if resposta == 1:
+        jogador.creditos -= 4
+        jogar_dados_revanche()
+        jogador.revanche1 = True
+    else:
+        menu()
+
+
+def jogar_dados_revanche():
+    jogador.revanche1 = True
+    lista = [1, 2, 3, 4, 5, 6]
+    print('\n************** A REVANCHE VAI COMEÇAR *************')
+    num_jogador = int(input(f"{jogador.nome}, digite o valor do dado (1-6): "))
+    while True:
+        if num_jogador > 6 or num_jogador < 1:
+            num_jogador = int(input("Digite um valor de 1-6: "))
         else:
             print("O computador está escolhendo um número ...")
             time.sleep(1)
@@ -27,68 +105,28 @@ def inicio():
             time.sleep(1)
             jogar_dados(num_jogador, num_comp)
             break
+    jogador.revanche1 = True
 
-def jogar_dados(num_jogador, num_comp):
-    print("\n ---------- OS DADOS FORAM LANÇADOS ---------- ")
-    time.sleep(1)
-    print("             *    SORTEANDO    *                 ")
-    time.sleep(2)
-    num_dados = random.randint(1, 6)
-    print(f'************ O numero do dado é: {num_dados} *************')
-    print('-' * 48)
-    verificar(num_dados, num_jogador, num_comp)
 
-def verificar(num_dados, num_jogador, num_comp):
-    if num_dados == num_jogador:
-        print(f'{jogador.nome}, VOCÊ VENCEU !!!')
-        jogador.creditos += 4
-        print(f'Você ganhou R$ 4,00 de Crédito. Seu crédito atual é de: {jogador.creditos}')
-        # vencer = False
-        # revanche(vencer)
-        menu()
-
-    elif num_dados == num_comp:
-        print('O Computador venceu essa partida !! ')
-        menu()
-    else:
-        print('Ninguém ganhou essa rodada, os dados serão jogados novamente')
-        time.sleep(3)
-        jogar_dados(num_jogador, num_comp)
-
-def revanche(vencer):
-    vencer = False
-    print('\nO computador sugeriu uma revanche! O dobro ou nada')
-    print('Se você vencer você ganha mais R$ 4,00 de créditos para jogar')
-    resposta = int(input('\n[1] Sim'
-                         '\n[2] Não'
-                         '\nDigite o número referente a sua resposta:'))
-    if resposta == 1:
-        print('************** A REVANCHE VAI COMEÇAR *************')
-        inicio()
-        if vencer == True:
-            jogador.creditos += 4
-            print('Você ganhou mais 4 reais de credito')
-
-    else:
-        menu()
 
 def menu():
-    print('MENU')
-
-
+    while True:
+        print('\n---------------- MENU ----------------'
+              '\n[1]- Jogar novamente'
+              '\n[2]- Encerrar')
+        resposta = int(input('Digite a opção desejada: '))
+        if resposta == 1:
+            inicio()
+        else:
+            break
 
 
 if __name__ == '__main__':
-    jogador = Jogo(input("Digite seu nome: ").title(), int(input("Cada partida são R$2,00.\nDigite o valor de crédito que deseja jogar: ")))
+    jogador = Jogo(input("Digite seu nome: ").title(),
+                   int(input("Cada partida são R$2,00.\nDigite o valor de crédito que deseja jogar: ")))
     while True:
-        if jogador.creditos %2 != 0 :
+        if jogador.creditos % 2 != 0:
             jogador.creditos = int(input('Digte um válor de numero par, por favor: '))
         else:
             inicio()
             break
-
-
-
-
-
-
